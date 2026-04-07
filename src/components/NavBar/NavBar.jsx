6290b50f06fe7df077986e4c2e5e64d705ec5d31
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2026-04-07 14:47:27 +0530

    fix: navbar issue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
+import { Menu, Phone, X } from "lucide-react";
 import { useState } from "react";
-import { Phone, Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
